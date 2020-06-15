b356c2b0b8724b818eb3eb2309c7f459abe2e396
evertonm <everton.martins1995@gmail.com>
2020-06-15 13:41:13 -0300

    new pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,21 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import FortgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
+import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
-    <Route path="/signup" exact component={SignUp} />
+    <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={FortgotPassword} />
+    <Route path="/reset-password" component={ResetPassword} />
 
-    <Route path="/dashboard" exact component={Dashboard} isPrivate />
+    <Route path="/profile" component={Profile} isPrivate />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
